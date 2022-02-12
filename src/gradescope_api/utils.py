25fcf573912d5087113e0dc5d8f9f8ff9e995b4a
Shomil Jain <shomil@berkeley.edu>
2022-02-12 10:15:18 -0800

    [fix] updated error for get_url_id

--- src/gradescope_api/utils.py
+++ src/gradescope_api/utils.py
@@ -1,2 +1,8 @@
+from gradescope_api.errors import GradescopeAPIError
+
+
 def get_url_id(url: str, kind: str) -> str:
-    return url.split(f"/{kind}/")[1].split("/")[0]
+    try:
+        return url.split(f"/{kind}/")[1].split("/")[0]
+    except Exception:
+        raise GradescopeAPIError(f"Gradescope URL is not properly formatted: {url}")
