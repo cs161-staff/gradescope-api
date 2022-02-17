0dee74aa6570f16fdc58553820967ae0269947a2
Shomil Jain <shomil@berkeley.edu>
2022-02-16 22:22:45 -0800

    Added better endpoint for extensions

--- src/gradescope_api/assignment.py
+++ src/gradescope_api/assignment.py
@@ -1,9 +1,14 @@
 from __future__ import annotations
 
-from datetime import datetime
+import json
+from datetime import datetime, timedelta
 from typing import TYPE_CHECKING, Optional
 
-from gradescope_api.errors import check_response
+import pytz
+from bs4 import BeautifulSoup
+from dateutil.parser import parse
+
+from gradescope_api.errors import GradescopeAPIError, check_response
 
 if TYPE_CHECKING:
     from gradescope_api.client import GradescopeClient
@@ -21,6 +26,63 @@ class GradescopeAssignment:
     def get_url(self) -> str:
         return self._course.get_url() + f"/assignments/{self.assignment_id}"
 
+    def apply_extension(self, email: str, num_days: int):
+        """
+        A new method to apply an extension to a Gradescope assignment, given an email and a number of days.
+        """
+        # First, fetch the extensions page for the assignment, which contains a student roster as well as
+        # the due date (and hard due date) for the assignment.
+        course_id = self._course.course_id
+        assignment_id = self.assignment_id
+        response = self._client.session.get(
+            f"https://www.gradescope.com/courses/{course_id}/assignments/{assignment_id}/extensions", timeout=20
+        )
+        check_response(response, "could not load assignment")
+
+        # Once we fetch the page, parse out the data (students + due dates)
+        soup = BeautifulSoup(response.content, "html.parser")
+        props = soup.find("li", {"data-react-class": "AddExtension"})["data-react-props"]
+        data = json.loads(props)
+        students = {row["email"]: row["id"] for row in data.get("students", [])}
+        user_id = students.get(email)
+        if not user_id:
+            raise GradescopeAPIError("student email not found")
+
+        # A helper method to transform the date
+        def transform_date(datestr: str):
+            dt = pytz.timezone("US/Pacific").localize(parse(datestr))
+            dt = dt + timedelta(num_days)
+            return dt.astimezone(pytz.utc)
+
+        assignment = data["assignment"]
+        new_due_date = transform_date(assignment["due_date"])
+        new_hard_due_date = transform_date(assignment["hard_due_date"])
+
+        # Make the post request to create the extension
+        url = self.get_url() + "/extensions"
+        headers = {
+            "Host": "www.gradescope.com",
+            "Origin": "https://www.gradescope.com",
+            "Referer": url,
+            "X-CSRF-Token": self._client._get_token(url, meta="csrf-token"),
+        }
+        payload = {
+            "override": {
+                "user_id": user_id,
+                "settings": {
+                    "due_date": {"type": "absolute", "value": new_due_date.strftime(GRADESCOPE_DATETIME_FORMAT)},
+                    "hard_due_date": {
+                        "type": "absolute",
+                        "value": new_hard_due_date.strftime(GRADESCOPE_DATETIME_FORMAT),
+                    },
+                },
+            }
+        }
+
+        response = self._client.session.post(url, headers=headers, json=payload, timeout=20)
+        check_response(response, "creating an extension failed")
+
+    # deprecated
     def create_extension(self, user_id: str, due_date: datetime, hard_due_date: Optional[datetime] = None):
         """
         Create an extension for a student for this particular assignment. If a hard due date is not provided,
--- src/test.py
+++ src/test.py
@@ -7,5 +7,8 @@ load_dotenv()
 
 client = GradescopeClient(email=os.environ["GS_EMAIL"], password=os.environ["GS_PASSWORD"])
 course = client.get_course(course_url="https://www.gradescope.com/courses/56746/")
-roster = course.get_roster()
-print(len(roster))
+assignment = course.get_assignment(assignment_url="https://www.gradescope.com/courses/56746/assignments/942482/review_grades")
+assignment.apply_extension('shomil+cs161test@berkeley.edu', 10)
+
+# roster = course.get_roster()
+# print(len(roster))
